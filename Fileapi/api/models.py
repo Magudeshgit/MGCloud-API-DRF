2fba5435b9b218917dd0e125b2632c4d710a0184
Magudesh K <74949557+Magudeshgit@users.noreply.github.com>
2024-06-19 13:39:42 +0530

    Delete Fileapi directory

--- Fileapi/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-class FileUpload(models.Model):
-	foreign_key=models.ForeignKey(User,related_name='Key',on_delete=models.CASCADE,null=True)
-	file = models.FileField(upload_to="UserFiles")
-	def __str__(self):
-		return self.file.name
